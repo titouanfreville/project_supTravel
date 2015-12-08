ff2ea91b5b81379e0530d87ea84ac68203d326d2
titouanfreville <titouanfreville@hotmail.fr>
2015-12-08 22:57:04 +0100

    UPDATE Corrected links and database register issue from added session. READY TO MERGE.

--- workspace/SupTravelTest/src/com/supinfo/suptravel/controller/BookTripServlet.java
+++ workspace/SupTravelTest/src/com/supinfo/suptravel/controller/BookTripServlet.java
@@ -17,7 +17,7 @@ import com.supinfo.suptravel.dao.UserDAO;
 /**
  * Servlet implementation class BookTripServlet
  */
-@WebServlet("/BookTrip")
+@WebServlet("/connected/BookTrip")
 public class BookTripServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Cookie[] cookie;
