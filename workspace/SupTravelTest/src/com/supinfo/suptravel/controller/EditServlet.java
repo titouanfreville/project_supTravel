5a9172cd8c810322a63cecdf8bca6b003d5dbe84
titouanfreville <titouanfreville@hotmail.fr>
2015-12-09 15:03:02 +0100

    UPDATE Added default value to edit. TODO Make it work :p

---
+++ workspace/SupTravelTest/src/com/supinfo/suptravel/controller/EditServlet.java
@@ -0,0 +1,61 @@
+package com.supinfo.suptravel.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.supinfo.suptravel.bean.User;
+import com.supinfo.suptravel.dao.TripbagDAO;
+import com.supinfo.suptravel.dao.UserDAO;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/connected/Edit")
+public class EditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		UserDAO udao = new UserDAO();
+        Cookie[] cookie = request.getCookies();
+        System.out.println("------------------------------------------\n"+cookie+"-----------------------------------------------------------\n");
+        for (Cookie c : cookie) {System.out.println(c);
+        	if (c.getName() != null && c.getName().equals("user_id")) {
+        		int id = Integer.parseInt(c.getValue());
+        		System.out.println(id);
+        		User u = udao.getUserObject(id);        		
+        		System.out.println("Shrauihruaize\n"+u+"\nazeuinhazeuiohazo\n");
+        		request.setAttribute("ltrip",u);
+    			getServletConfig().getServletContext().getRequestDispatcher("/connected/mytrips.jsp").forward(request,response);
+        	} 
+        }
+	}
+
+}
