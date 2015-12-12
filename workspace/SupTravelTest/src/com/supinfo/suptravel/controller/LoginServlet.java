fb1cb17a4560508f59fd22bd984f8fc2d56b446c
titouanfreville <titouanfreville@hotmail.fr>
2015-12-12 16:44:18 +0100

    UPDATE : made search REST output in jsp

--- workspace/SupTravelTest/src/com/supinfo/suptravel/controller/LoginServlet.java
+++ workspace/SupTravelTest/src/com/supinfo/suptravel/controller/LoginServlet.java
@@ -1,7 +1,6 @@
 package com.supinfo.suptravel.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,7 +20,12 @@ import com.supinfo.suptravel.dao.UserDAO;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private HttpSession session;
-       
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());doGet(request, response);
+		
+	} 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
